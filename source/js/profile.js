24fcf1aeeed56b9c8dcfdb818f92ff2d7d12d75e
FilosofiaJulia <filosofiajulia@gmail.com>
2023-05-16 15:10:19 +0300

    add rerender links on profile

--- source/js/profile.js
+++ source/js/profile.js
@@ -4,3 +4,15 @@ popupHandler('password-form__popup_js', 'profile__change-password-btn_js', 'pass
 // вызов функции открытия и закрытия редактирования данных профиля
 popupHandler('form-edit-data__popup_js', 'profile__change-data-btn_js', 'form-edit-data__btn-close_js');
 
+// функция выделение текущей страницы как активной
+(function() {
+    const currentPage = document.querySelector('.header__profile-link_js');
+    const currentBurgerPage = document.querySelector('.header__burger-profile-link_js');
+    currentPage.classList.add('header__link_active');
+    currentBurgerPage.classList.add('header__link_active');
+})();
+// Инициализация ссылок в меню при аутентификации
+(function setMenuLinks() {
+    rerenderLinks();
+    rerenderBurgerLinks();
+})();
